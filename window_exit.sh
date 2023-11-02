0032959f7513a6f3254f368a03193062a5fbb5c6
Joseph <119084558+DerjenigeUberMensch@users.noreply.github.com>
2023-11-02 00:04:12 +0000

    Delete window_exit.sh

--- window_exit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get the ID of the currently active (top) window using xprop
-active_window_id=$(xprop -root | grep "_NET_ACTIVE_WINDOW(WINDOW)" | awk '{print $5}')
-
-if [[ -n "$active_window_id" ]]; then
-     xdotool getwindowfocus windowclose
-fi
