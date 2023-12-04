b397b05f9bfdcccf415231c97b925dd2376b1aa1
Joseph <119084558+DerjenigeUberMensch@users.noreply.github.com>
2023-12-04 01:22:16 +0000

    Delete parser.h
    Useless as of restartsig

--- parser.h
+++
@@ -1,9 +0,0 @@
-static char**
-parseline(char *buffText);
-static void parsefile(char *filename);
-static int checkconf(char *name, char *value);
-static int setconf(char *name, char *value);
-static char **
-createArray(int size);
-static void readconfig();
-static void logError(const char *text);
