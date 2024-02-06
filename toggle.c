78a84e83c66df7c6bfdc8613e1b42d14c9281a85
Joseph <119084558+DerjenigeUberMensch@users.noreply.github.com>
2024-02-06 02:15:24 +0000

    Reordered toggle.h documentation
    toggle.c docu -> toggle.h docu

--- toggle.c
+++ toggle.c
@@ -41,7 +41,6 @@
  * SomeToggleFunction(&arg);
  */
 
-/* Mostly a testing function */
 void
 UserStats(const Arg *arg)
 {
@@ -50,7 +49,6 @@ UserStats(const Arg *arg)
     somenum ^= 1;
 }
 
-/* Switch to a monitor based on the argument int arg i */
 void
 FocusMonitor(const Arg *arg)
 {
@@ -63,7 +61,6 @@ FocusMonitor(const Arg *arg)
     focus(NULL);
 }
 
-/* Changes the max number of master windows possible */
 void
 ChangeMasterWindow(const Arg *arg)
 {
@@ -71,21 +68,18 @@ ChangeMasterWindow(const Arg *arg)
     arrange(selmon);
 }
 
-/* Kills the current window */
 void
 KillWindow(const Arg *arg)
 {
     killclient(selmon->sel, Graceful);
 }
 
-/* Attempts to kill the current window directly instead of just sending a signal and waiting for the window to respond */
 void
 TerminateWindow(const Arg *arg)
 {
     killclient(selmon->sel, Destroy);
 }
 
-/* keybind to move the current window where the mouse cursor is */
 void
 DragWindow(const Arg *arg) /* movemouse */
 {
@@ -153,7 +147,6 @@ DragWindow(const Arg *arg) /* movemouse */
     restack(selmon);
 }
 
-/* restarts dwm */
 void
 Restart(const Arg *arg)
 {
@@ -161,7 +154,6 @@ Restart(const Arg *arg)
     restart();
 }
 
-/* quits dwm */
 void
 Quit(const Arg *arg)
 {
@@ -169,7 +161,6 @@ Quit(const Arg *arg)
     quit();
 }
 
-/* resizes the current window based on mouse position */
 void
 ResizeWindow(const Arg *arg) /* resizemouse */
 {
@@ -281,7 +272,6 @@ ResizeWindow(const Arg *arg) /* resizemouse */
     restack(selmon);
 }
 
-/* sets the window layout based on a enum in dwm.h -> Grid, Floating, Monocle, Tiled */
 void
 SetWindowLayout(const Arg *arg)
 {
@@ -304,8 +294,6 @@ SetWindowLayout(const Arg *arg)
     }
 }
 
-/* Sets the size of the master window 0.0 -> 1.0 for the Tiled Layout
- * where 1 is just monocle with extra steps */
 void
 SetMonitorFact(const Arg *arg)
 {
@@ -332,8 +320,6 @@ SetMonitorFact(const Arg *arg)
  * arg.v = variablename;
  * SpawnWindow(&arg);
  */
-
-/* Spawns a window based on arguments provided */
 void
 SpawnWindow(const Arg *arg)
 {
@@ -346,7 +332,6 @@ SpawnWindow(const Arg *arg)
     }
 }
 
-/* Maximizes the currently selected window */
 void
 MaximizeWindow(const Arg *arg)
 {
@@ -373,7 +358,6 @@ MaximizeWindow(const Arg *arg)
     }
 }
 
-/* Maximizes a window vertically */ 
 void
 MaximizeWindowVertical(const Arg *arg) 
 {
@@ -394,7 +378,6 @@ MaximizeWindowVertical(const Arg *arg)
     }
 }
 
-/* Maximizes a window horizontally */
 void
 MaximizeWindowHorizontal(const Arg *arg) 
 {
@@ -420,7 +403,6 @@ MaximizeWindowHorizontal(const Arg *arg)
  * producing a somewhat simple (in theory) alttab function
  */
 
-/* Switches to the next visible window based on user input */
 void
 AltTab(const Arg *arg)
 {
@@ -494,7 +476,6 @@ AltTab(const Arg *arg)
     XUngrabPointer(dpy, CurrentTime);
 }
 
-/* Tags a window AKA makes it visible in another desktop thing or "tag" */
 void
 TagWindow(const Arg *arg)
 {
@@ -506,7 +487,6 @@ TagWindow(const Arg *arg)
     }
 }
 
-/* Tags a monitor */
 void
 TagMonitor(const Arg *arg)
 {
@@ -514,7 +494,6 @@ TagMonitor(const Arg *arg)
     sendmon(selmon->sel, dirtomon(arg->i));
 }
 
-/* Toggles if we show the Status bar or not */
 void
 ToggleStatusBar(const Arg *arg)
 {
@@ -524,7 +503,6 @@ ToggleStatusBar(const Arg *arg)
     arrange(selmon);
 }
 
-/* This toggles the floating layout */
 void
 ToggleFloating(const Arg *arg)
 {
@@ -544,7 +522,6 @@ ToggleFloating(const Arg *arg)
     arrange(selmon);
 }
 
-/* Toggles fullscreen mode for all windows in current tag */
 void
 ToggleFullscreen(const Arg *arg)
 {
@@ -569,7 +546,6 @@ ToggleFullscreen(const Arg *arg)
     arrange(m);
 }
 
-/* Probably toggles the tag selected IDK */
 void
 ToggleTag(const Arg *arg)
 {
@@ -586,11 +562,6 @@ ToggleTag(const Arg *arg)
     updatedesktop();
 }
 
-/* This is used in keybinds default SUPER + (1-9)
- * And essential just changes the tag to the one specified in the number
- * AKA 1 << TAG
- * This is really only a "User function" see View for devs
- */
 void
 ToggleView(const Arg *arg)
 {
@@ -605,11 +576,6 @@ ToggleView(const Arg *arg)
     }
 }
 
-/* Switches to the tag number specified in base power of 2^x
- * Example:
- * tag1,tag2,tag3,tag4,tag5,tag6,tag7,tag8,tag9
- * {1,  2,   4,   8,   16,  32,  64,  128, 256}
- */
 void
 View(const Arg *arg)
 {
@@ -621,7 +587,6 @@ View(const Arg *arg)
     updatedesktop();
 }
 
-/* Idk what this does */
 void
 Zoom(const Arg *arg)
 {
